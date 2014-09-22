dbdbf25bc2a959f5615f2d6e221f78842eabf441
Seaward Chen <seaward@gmail.com>
2014-09-22 11:29:29 +0800

    Finished project requirements. But the `dsMean` is obtained  in an awkward way

--- CodeBook.md
+++ CodeBook.md
@@ -7,18 +7,6 @@ output: html_document
 
 Run ```{r}run_analysis``` to generate tidy data from collected accelerometer data from Samsung Galaxy S.
 
-The accelerometer data is here: <https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip>. Download it to working directory, unzip it, then run ```{r}run_analysis``` to generate tiday datasets.
+The accelerometer data is here: <https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip>. Download it to working directory, unzip it, then run ```{r}run_analysis``` to generate tidy datasets.
 
-When you click the **Knit** button a document will be generated that includes both content as well as the output of any embedded R code chunks within the document. You can embed an R code chunk like this:
-
-```{r}
-summary(cars)
-```
-
-You can also embed plots, for example:
-
-```{r, echo=FALSE}
-plot(cars)
-```
-
-Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.
+The result is a list of two `data.frame`s, `ds` is the data selected from accelerometer dataset, which include all sd and mean variables; distilled from `ds`, `dsMean` has the mean of all sd and mean variables by subject and activity.
--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,11 @@
+### Get tidy data of all sd and mean variables of accelerometer dataset,
+### also generate a dataset of mean of each variable by each subject and each activity.
+### Download accelerometer dataset zip file, unzip to
+### current directory, then run this script to generate
+### tidy data.
 run_analysis <- function() {
+  
+  library(dplyr)
   
   actLabels = read.table("UCI HAR Dataset/activity_labels.txt", sep = " ")
 
@@ -7,34 +14,118 @@ run_analysis <- function() {
   cols = grep("-(mean)|(std)\\(\\)", features$V2)
   f = rep("X16", length(features[[1]]))
   f[cols] = "F16"
+
+  ## Update features into accessible strings
+  actNames = features[cols, 2]
+  actNames = gsub("-", ".", actNames)
+  actNames = sub("\\(\\)", "", actNames)
   
   type = "train"
   activity = actLabeledDataset(actLabels, type)
   subject = read.table(sprintf("UCI HAR Dataset/%s/subject_%s.txt", type, type))
   names(subject) = "subject"
-  dsTrain = featuredDataset(features, cols, f, type)
+  dsTrain = featuredDataset(actNames, f, type)
   dsTrain = cbind(subject, activity, dsTrain)
   
   type = "test"
   activity = actLabeledDataset(actLabels, type)
   subject = read.table(sprintf("UCI HAR Dataset/%s/subject_%s.txt", type, type))
   names(subject) = "subject"
-  dsTest = featuredDataset(features, cols, f, type)
+  dsTest = featuredDataset(actNames, f, type)
   dsTest = cbind(subject, activity, dsTest)
   
   ds = rbind(dsTrain, dsTest)
+  dsLen = length(ds)
   
-  ds
+  ds = group_by(ds, subject, activity)
+  dsMean = mutate(ds, mean(tBodyAcc.mean.X)
+                  ,mean(tBodyAcc.mean.Y)
+                  ,mean(tBodyAcc.mean.Z)
+                  ,mean(tBodyAcc.std.X)
+                  ,mean(tBodyAcc.std.Y)
+                  ,mean(tBodyAcc.std.Z)
+                  ,mean(tGravityAcc.mean.X)
+                  ,mean(tGravityAcc.mean.Y)
+                  ,mean(tGravityAcc.mean.Z)
+                  ,mean(tGravityAcc.std.X)
+                  ,mean(tGravityAcc.std.Y)
+                  ,mean(tGravityAcc.std.Z)
+                  ,mean(tBodyAccJerk.mean.X)
+                  ,mean(tBodyAccJerk.mean.Y)
+                  ,mean(tBodyAccJerk.mean.Z)
+                  ,mean(tBodyAccJerk.std.X)
+                  ,mean(tBodyAccJerk.std.Y)
+                  ,mean(tBodyAccJerk.std.Z)
+                  ,mean(tBodyGyro.mean.X)
+                  ,mean(tBodyGyro.mean.Y)
+                  ,mean(tBodyGyro.mean.Z)
+                  ,mean(tBodyGyro.std.X)
+                  ,mean(tBodyGyro.std.Y)
+                  ,mean(tBodyGyro.std.Z)
+                  ,mean(tBodyGyroJerk.mean.X)
+                  ,mean(tBodyGyroJerk.mean.Y)
+                  ,mean(tBodyGyroJerk.mean.Z)
+                  ,mean(tBodyGyroJerk.std.X)
+                  ,mean(tBodyGyroJerk.std.Y)
+                  ,mean(tBodyGyroJerk.std.Z)
+                  ,mean(tBodyAccMag.mean)
+                  ,mean(tBodyAccMag.std)
+                  ,mean(tGravityAccMag.mean)
+                  ,mean(tGravityAccMag.std)
+                  ,mean(tBodyAccJerkMag.mean)
+                  ,mean(tBodyAccJerkMag.std)
+                  ,mean(tBodyGyroMag.mean)
+                  ,mean(tBodyGyroMag.std)
+                  ,mean(tBodyGyroJerkMag.mean)
+                  ,mean(tBodyGyroJerkMag.std)
+                  ,mean(fBodyAcc.mean.X)
+                  ,mean(fBodyAcc.mean.Y)
+                  ,mean(fBodyAcc.mean.Z)
+                  ,mean(fBodyAcc.std.X)
+                  ,mean(fBodyAcc.std.Y)
+                  ,mean(fBodyAcc.std.Z)
+                  ,mean(fBodyAcc.meanFreq.X)
+                  ,mean(fBodyAcc.meanFreq.Y)
+                  ,mean(fBodyAcc.meanFreq.Z)
+                  ,mean(fBodyAccJerk.mean.X)
+                  ,mean(fBodyAccJerk.mean.Y)
+                  ,mean(fBodyAccJerk.mean.Z)
+                  ,mean(fBodyAccJerk.std.X)
+                  ,mean(fBodyAccJerk.std.Y)
+                  ,mean(fBodyAccJerk.std.Z)
+                  ,mean(fBodyAccJerk.meanFreq.X)
+                  ,mean(fBodyAccJerk.meanFreq.Y)
+                  ,mean(fBodyAccJerk.meanFreq.Z)
+                  ,mean(fBodyGyro.mean.X)
+                  ,mean(fBodyGyro.mean.Y)
+                  ,mean(fBodyGyro.mean.Z)
+                  ,mean(fBodyGyro.std.X)
+                  ,mean(fBodyGyro.std.Y)
+                  ,mean(fBodyGyro.std.Z)
+                  ,mean(fBodyGyro.meanFreq.X)
+                  ,mean(fBodyGyro.meanFreq.Y)
+                  ,mean(fBodyGyro.meanFreq.Z)
+                  ,mean(fBodyAccMag.mean)
+                  ,mean(fBodyAccMag.std)
+                  ,mean(fBodyAccMag.meanFreq)
+                  ,mean(fBodyBodyAccJerkMag.mean)
+                  ,mean(fBodyBodyAccJerkMag.std)
+                  ,mean(fBodyBodyAccJerkMag.meanFreq)
+                  ,mean(fBodyBodyGyroMag.mean)
+                  ,mean(fBodyBodyGyroMag.std)
+                  ,mean(fBodyBodyGyroMag.meanFreq)
+                  ,mean(fBodyBodyGyroJerkMag.mean)
+                  ,mean(fBodyBodyGyroJerkMag.std)
+                  ,mean(fBodyBodyGyroJerkMag.meanFreq))
+  dsMeanLen = length(dsMean)
+  dsMean = dsMean[,c(1,2, (dsLen+1) : dsMeanLen)]
+  
+  res = list(ds = ds, dsMean = unique(dsMean))
 }
 
-featuredDataset <- function (features, cols, f, type = "train") {
-  ## Column names on dataset
-  ds = read.fortran(sprintf("UCI HAR Dataset/%s/X_%s.txt", type, type), f)
-  names(ds) = features[cols, 2]
-  
-  ds
-}
-
+## Get list of activity names
+## param actLabels - activity labels
+## param type - train / test
 actLabeledDataset <- function (actLabels, type = "train") {
   ## Activity column
   act = read.table(sprintf("UCI HAR Dataset/%s/y_%s.txt", type, type))
@@ -45,4 +136,17 @@ actLabeledDataset <- function (actLabels, type = "train") {
   }
   
   actStr
+}
+
+## The activities dataset with column names
+## param features - feature names
+## param cols - the selected columns number
+## param f - Format list for reading from fixed-format data file
+## param type - train / test data type.
+featuredDataset <- function (actNames, f, type = "train") {
+  ds = read.fortran(sprintf("UCI HAR Dataset/%s/X_%s.txt", type, type), f)
+  ## Set column names on dataset
+  names(ds) = actNames
+  
+  ds
 }
